71b73a946066dcc22c8e4103fd5760afcd74cc62
kojou82877 <116699713+kojou82877@users.noreply.github.com>
2023-02-20 12:26:40 +0530

    Update kojouxddd.py

--- kojouxddd.py
+++ kojouxddd.py
@@ -36,7 +36,7 @@ def login():
     f.close()
     browser.select_form(nr = 0)
     print("\33[1;33;40m", end = "")
-    sp("\Enter the Api graph")
+    sp("\Enter the api graph")
     print("\33[1;37;40m")
     apr = str(input())
     try:
